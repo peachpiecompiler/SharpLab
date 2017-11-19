74a72e9c3fd5fdfc7289027df00ea45d2a49c41b
Andrey Shchekin <ashmind@gmail.com>
2017-11-19 21:02:55 +1300

    First part of upgrade work.

--- source/Server/Common/TargetNames.cs
+++ source/Server/Common/TargetNames.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
 
 namespace SharpLab.Server.Common {
     public static class TargetNames {
+        public const string CSharp = LanguageNames.CSharp;
         public const string Ast = "AST";
         public const string JitAsm = "JIT ASM";
         public const string Run = "Run";
--- source/Server/Decompilation/CSharpDecompiler.cs
+++ source/Server/Decompilation/CSharpDecompiler.cs
@@ -1,16 +1,42 @@
-﻿using System.IO;
-using ICSharpCode.Decompiler.Ast;
+using System.Collections.Concurrent;
+using System.IO;
+using ICSharpCode.Decompiler;
+using ICSharpCode.Decompiler.CSharp.OutputVisitor;
 using Microsoft.CodeAnalysis;
-using SharpLab.Server.Decompilation.Internal;
+using Mono.Cecil;
+using SharpLab.Server.Common;
 
 namespace SharpLab.Server.Decompilation {
-    public class CSharpDecompiler : AstBasedDecompiler {
-        protected override void WriteResult(TextWriter writer, AstBuilder ast) {
-            ast.GenerateCode(new CustomizableIndentPlainTextOutput(writer) {
-                IndentationString = "    "
+    public class CSharpDecompiler : IDecompiler {
+        private static readonly ConcurrentDictionary<string, AssemblyDefinition> AssemblyCache = new ConcurrentDictionary<string, AssemblyDefinition>();
+        private static readonly CSharpFormattingOptions FormattingOptions = FormattingOptionsFactory.CreateKRStyle();
+        private static readonly DecompilerSettings DecompilerSettings = new DecompilerSettings {
+            //CanInlineVariables = false,
+            //OperatorOverloading = false,
+            AnonymousMethods = false,
+            AnonymousTypes = false,
+            YieldReturn = false,
+            AsyncAwait = false,
+            AutomaticProperties = false,
+            ExpressionTrees = false,
+            //ArrayInitializers = false,
+            ObjectOrCollectionInitializers = false,
+            //LiftedOperators = false,
+            UsingStatement = false
+        };
+
+        public void Decompile(Stream assemblyStream, TextWriter codeWriter) {
+            // ReSharper disable once AgentHeisenbug.CallToNonThreadSafeStaticMethodInThreadSafeType
+            var module = ModuleDefinition.ReadModule(assemblyStream, new ReaderParameters {
+                AssemblyResolver = PreCachedAssemblyResolver.Instance
             });
+
+            var decompiler = new ICSharpCode.Decompiler.CSharp.CSharpDecompiler(module, DecompilerSettings);
+            var syntaxTree = decompiler.Decompile();
+
+            new CSharpOutputVisitor(codeWriter, FormattingOptions).VisitSyntaxTree(syntaxTree);
         }
 
-        public override string LanguageName => LanguageNames.CSharp;
+        public string LanguageName => TargetNames.CSharp;
     }
 }
