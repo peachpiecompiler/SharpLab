3ffaaec248f5cf8b78b9f908ce3d75e29932ee8b
Robert Husák <robert@husak.cloud>
2018-01-17 15:27:44 +0100

    Display TypeDecl properties in PHP syntax tree

--- source/Server/Decompilation/AstOnly/PhpAstTarget.cs
+++ source/Server/Decompilation/AstOnly/PhpAstTarget.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Devsense.PHP.Syntax;
 using Devsense.PHP.Syntax.Ast;
+using Devsense.PHP.Text;
 using MirrorSharp.Advanced;
 using MirrorSharp.Php.Advanced;
 using SharpLab.Server.Decompilation.Internal;
@@ -32,9 +34,7 @@ namespace SharpLab.Server.Decompilation.AstOnly
 
                     _writer.WriteProperty("type", "node");
                     _writer.WriteProperty("kind", element.GetType().Name);
-
-                    _writer.WritePropertyName("range");
-                    _writer.WriteValueFromParts(element.Span.Start, '-', element.Span.End);
+                    SerializeSpanProperty(element.Span);
 
                     // Visit children
                     _firstChild = true;
@@ -50,6 +50,70 @@ namespace SharpLab.Server.Decompilation.AstOnly
 
                     _writer.WriteEndObject();
                 }
+            }
+
+            public override void VisitTypeDecl(TypeDecl x) {
+                VisitNodeProlog();
+
+                SerializeToken(nameof(x.Name), x.Name.ToString(), x.Name.Span);
+                SerializeToken(nameof(x.BaseClass), x.BaseClass?.ClassName.ToString(), x.BaseClass?.Span);
+                SerializeToken(nameof(x.MemberAttributes), x.MemberAttributes.ToString(), x.HeadingSpan);
+                SerializeTokenList(nameof(x.ImplementsList), x.ImplementsList, impl => impl.ClassName.ToString(), impl => impl.Span);
+                SerializeToken(nameof(x.IsConditional), x.IsConditional.ToString(), null);
+
+                base.VisitTypeDecl(x);
+            }
+
+            private void VisitNodeProlog() {
+                if (_firstChild) {
+                    // If this element is the first child of another one, start writing its 'children' property
+                    _writer.WritePropertyStartArray("children");
+
+                    // Do not repeat the 'children' property header in the children nodes
+                    _firstChild = false;
+                }
+            }
+
+            private void SerializeToken(string name, string value, Span? span) {
+                if (value != null) {
+                    _writer.WriteStartObject();
+                    _writer.WriteProperty("type", span.HasValue ? "token" : "value");
+                    if (name != null) {
+                        _writer.WriteProperty("property", name);
+                    }
+                    _writer.WriteProperty("value", value);
+                    if (span != null) {
+                        SerializeSpanProperty(span.Value);
+                    }
+                    _writer.WriteEndObject();
+                }
+            }
+
+            private void SerializeTokenList<T>(string name, IEnumerable<T> items, Func<T, string> valueSelector, Func<T, Span> spanSelector = null) {
+                if (items.Any()) {
+                    _writer.WriteStartObject();
+                    _writer.WriteProperty("type", (spanSelector != null) ? "token" : "value");
+                    _writer.WriteProperty("property", name);
+
+                    if (spanSelector != null) {
+                        int minStart = items.Min(item => spanSelector(item).Start);
+                        int maxEnd = items.Max(item => spanSelector(item).End);
+                        var span = new Span(minStart, maxEnd - minStart);
+                        SerializeSpanProperty(span);
+                    }
+
+                    _writer.WritePropertyStartArray("children");
+                    foreach (var item in items) {
+                        SerializeToken(null, valueSelector(item), spanSelector?.Invoke(item));
+                    }
+                    _writer.WriteEndArray();
+                    _writer.WriteEndObject();
+                }
+            }
+
+            private void SerializeSpanProperty(Span span) {
+                _writer.WritePropertyName("range");
+                _writer.WriteValueFromParts(span.Start, '-', span.End);
             }
         }
 
